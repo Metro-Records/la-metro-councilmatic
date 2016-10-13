f764c69a68b4fb20b32b327cd0b4fe3a4e9ede91
reginafcompton <reginafcompton@gmail.com>
2016-10-13 17:27:24 -0500

    Start PDF Viewer

--- lametro/models.py
+++ lametro/models.py
@@ -20,30 +20,43 @@ class LAMetroBill(Bill):
     # LA METRO CUSTOMIZATION
     @property
     def inferred_status(self):
-        # return 'Active'
-
         # TODO: Figure out when a bill is active or stale or if this matters at all. Pasted below comes from Chicago.
-
+        # We can expect only one action per bill. LA Metro does not provide a robust history.
         actions = self.actions.all().order_by('-order')
-        print(actions)
-        # classification_hist = [a.classification for a in actions]
-        # last_action_date = actions[0].date if actions else None
-        # bill_type = self.bill_type
 
-        # if bill_type.lower() in ['communication', 'oath of office']:
-        #     return None
+        description_hist = [a.description for a in actions]
+
+        last_action_date = actions[0].date if actions else None
+
+        bill_type = self.bill_type
+
+        if bill_type.lower() in ['informational report', 'public hearing', 'minutes', 'appointment', 'oral report / presentation', 'motion / motion response']:
+            return None
         # if self._terminal_status(classification_hist, bill_type):
         #     return self._terminal_status(classification_hist, bill_type)
         # elif self._is_stale(last_action_date):
         #     return 'Stale'
-        # else:
-        #     return 'Active'
-
-        return "Active"
-
+        else:
+            return 'Active'
 
     # LA METRO CUSTOMIZATION
     @property
     def attachments(self):
 
         return self.documents.filter(document_type='V').all()
+
+    # LA METRO CUSTOMIZATION
+    @property
+    def full_text_doc_url(self):
+        # base_url = 'https://pic.datamade.us/lametro/document/'
+        base_url = 'http://127.0.0.1:8000/pdfviewer/'
+
+        if self.documents.filter(document_type='V').all():
+            legistar_doc_url = self.documents.filter(document_type='V').first().document.url
+            doc_url = '{0}?filename={2}&document_url={1}'.format(base_url,
+                                                                 legistar_doc_url,
+                                                                 self.identifier)
+            return ({ 'legistar_doc_url': legistar_doc_url,
+              'doc_url': doc_url})
+        else:
+            return None
--- lametro/views.py
+++ lametro/views.py
@@ -5,11 +5,12 @@ from councilmatic_core.models import *
 from lametro.models import LAMetroBill
 
 class LABillDetail(BillDetailView):
-  model = LAMetroBill
-  template_name = 'lametro/legislation.html'
+    model = LAMetroBill
+    template_name = 'lametro/legislation.html'
 
-  def get_context_data(self, **kwargs):
-        context = super(BillDetailView, self).get_context_data(**kwargs)
-        context['actions'] = self.get_object().actions.all().order_by('-order')
+    def get_context_data(self, **kwargs):
+          context = super(BillDetailView, self).get_context_data(**kwargs)
+          context['actions'] = self.get_object().actions.all().order_by('-order')
 
-        return context
+          return context
+
