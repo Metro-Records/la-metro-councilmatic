825a68a3113e799b163e71c8fe7c1008777b7acc
reginafcompton <reginafcompton@gmail.com>
2016-10-13 10:41:32 -0500

    Stash

--- lametro/views.py
+++ lametro/views.py
@@ -5,3 +5,10 @@ from django.shortcuts import render
 
 class LABillDetail(BillDetailView):
   template_name = 'lametro/legislation.html'
+
+  def get_context_data(self, **kwargs):
+        context = super(BillDetailView, self).get_context_data(**kwargs)
+
+        print(context)
+
+        return context
