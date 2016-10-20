a20b496b247a75ef8e7fdae0985c7950e330f461
Eric van Zanten <eric.vanzanten@gmail.com>
2016-10-20 15:23:23 -0500

    Adding other deployment files

---
+++ scripts/before_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Make project directory if it doesn't exist. This is mainly to ensure that these scripts work on a bare server
+mkdir -p /home/datamade/la-metro-councilmatic
+
+# Decrypt files encrypted with blackbox
+cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/ && sudo -H -u datamade blackbox_postdeploy
