c09696243c8e1a177042c46490624578610e0ce3
Eric van Zanten <eric.vanzanten@gmail.com>
2016-10-20 16:05:38 -0500

    Maybe perms will work?

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,4 +4,4 @@
 mkdir -p /home/datamade/la-metro-councilmatic
 
 # Decrypt files encrypted with blackbox
-cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/ && chown datamade.datamade . && sudo -H -u datamade blackbox_postdeploy
+cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/ && chown -R datamade.datamade . && sudo -H -u datamade blackbox_postdeploy
