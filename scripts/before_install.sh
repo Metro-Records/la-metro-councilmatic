e86b01529e6c7b90092be3833a17e91dda1d43f8
Eric van Zanten <eric.vanzanten@gmail.com>
2016-10-21 16:05:29 -0500

    try again

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -7,10 +7,12 @@ mkdir -p /home/datamade/la-metro-councilmatic
 
 if [ "$DEPLOYMENT_GROUP_NAME" == "staging" ]
 then
+    rm -Rf /home/datamade/lametro-staging
     mkdir -p /home/datamade/lametro-staging
 fi
 if [ "$DEPLOYMENT_GROUP_NAME" == "production" ]
 then
+    rm -Rf /home/datamade/lametro
     mkdir -p /home/datamade/lametro
 fi
 
