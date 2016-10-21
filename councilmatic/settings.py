0f1a74df7bf045455db742cb421369dad82852b6
Eric van Zanten <eric.vanzanten@gmail.com>
2016-10-21 15:48:49 -0500

    Make sure static files and DB settings are correct

--- councilmatic/settings.py
+++ councilmatic/settings.py
@@ -103,7 +103,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
