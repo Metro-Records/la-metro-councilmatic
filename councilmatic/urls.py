f2834d33921138d5916a93907f6c5f4afcb62b24
reginafcompton <reginafcompton@gmail.com>
2016-10-24 14:47:20 -0500

    Create SQL to get committees

--- councilmatic/urls.py
+++ councilmatic/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.views.generic.base import RedirectView
 from haystack.query import SearchQuerySet
 from councilmatic_core.views import CouncilmaticSearchForm, CouncilmaticFacetedSearchView, EventDetailView
-from lametro.views import LABillDetail, LABoardMembersView, LAMetroAboutView, LACommitteeDetailView, LAPersonDetailView
+from lametro.views import LABillDetail, LABoardMembersView, LAMetroAboutView, LACommitteeDetailView, LACommitteesView, LAPersonDetailView
 
 sqs = SearchQuerySet().facet('bill_type')\
                       .facet('sponsorships', sort='index')\
@@ -35,6 +35,7 @@ urlpatterns = [
                                        form_class=CouncilmaticSearchForm), name='search'),
     url(r'^about/$', LAMetroAboutView.as_view(), name='about'),
     url(r'^legislation/(?P<slug>[^/]+)/$', LABillDetail.as_view(), name='bill_detail'),
+    url(r'^committees/$', LACommitteesView.as_view(), name='committees'),
     url(r'^committee/(?P<slug>[^/]+)/$', LACommitteeDetailView.as_view(), name='committee'),
     url(r'^board-members/$', LABoardMembersView.as_view(), name='council_members'),
     url(r'^person/(?P<slug>[^/]+)/$', LAPersonDetailView.as_view(), name='person'),
--- lametro/views.py
+++ lametro/views.py
@@ -1,11 +1,12 @@
 import re
+from datetime import datetime
 
 from django.conf import settings
 from django.shortcuts import render
 from django.db import connection
 from django.db.models.functions import Lower
 from collections import namedtuple
-from councilmatic_core.views import BillDetailView, CouncilMembersView, AboutView, CommitteeDetailView, PersonDetailView, EventDetailView
+from councilmatic_core.views import BillDetailView, CouncilMembersView, AboutView, CommitteeDetailView, CommitteesView, PersonDetailView, EventDetailView
 from councilmatic_core.models import *
 from lametro.models import LAMetroBill, LAMetroPost, LAMetroPerson
 
@@ -30,6 +31,48 @@ class LABoardMembersView(CouncilMembersView):
 
 class LAMetroAboutView(AboutView):
     template_name = 'lametro/about.html'
+
+class LACommitteesView(CommitteesView):
+
+    # this = Organization.objects.filter(classification='committee').order_by('name').filter(memberships__isnull=False).distinct()
+    # now = datetime.now()
+
+    # that = Organization.objects.filter(classification='committee').order_by('name').filter(memberships__isnull=False).filter(memberships__end_date__gte=now).distinct()
+
+    def get_queryset(self):
+        with connection.cursor() as cursor:
+
+            sql = ('''
+              SELECT DISTINCT o.name, o.slug
+              FROM councilmatic_core_organization AS o
+              JOIN councilmatic_core_membership AS m
+              ON o.ocd_id=m.organization_id
+              WHERE m.end_date::date > NOW()::date
+              ''')
+
+            cursor.execute(sql)
+
+            # committees = cursor.fetchall()
+
+            # print(committees)
+            # print(Organization.committees)
+            columns = [c[0] for c in cursor.description]
+            print(columns)
+
+            committees_tuple = namedtuple('Committee', columns)
+
+            print(cursor)
+
+            # committees = [committees_tuple(*c) for c in cursor]
+
+            # print(committees)
+
+            now = datetime.now()
+
+            committees = Organization.objects.filter(classification='committee').order_by('name').filter(memberships__isnull=False).filter(memberships__end_date__gte=now).distinct()
+
+
+            return committees
 
 class LACommitteeDetailView(CommitteeDetailView):
 
@@ -113,3 +156,4 @@ class LAPersonDetailView(PersonDetailView):
         # in django-councilmatic)
 
         return context
+
