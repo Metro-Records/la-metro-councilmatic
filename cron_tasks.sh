d577d7b9ffa3b22f8396f8fc054f160b722304f4
reginafcompton <reginafcompton@gmail.com>
2017-01-11 13:52:49 -0600

    Update crontask, and update view

--- cron_tasks.sh
+++ cron_tasks.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 cd /home/datamade/lametro-staging
-/home/datamade/.virtualenvs/lametro-staging/bin/python manage.py loaddata >> /tmp/lametro-loaddata.log 2>&1
+/home/datamade/.virtualenvs/lametro-staging/bin/python manage.py import_data >> /tmp/lametro-loaddata.log 2>&1
 /home/datamade/.virtualenvs/lametro-staging/bin/python manage.py update_index
