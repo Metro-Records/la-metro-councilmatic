2147140dab79b7c0d1efbd88fc4736a5a58bbcbe
reginafcompton <reginafcompton@gmail.com>
2017-03-10 16:02:54 -0600

    Approach solution with PyPDF2

--- lametro/management/commands/compile_pdfs.py
+++ lametro/management/commands/compile_pdfs.py
@@ -1,4 +1,7 @@
 import sqlalchemy as sa
+from PyPDF2 import PdfFileMerger, PdfFileReader
+from urllib.request import urlopen
+from io import StringIO, BytesIO
 
 from django.core.management.base import BaseCommand
 from django.conf import settings
@@ -19,11 +22,25 @@ class Command(BaseCommand):
 
         self.stdout.write(self.style.NOTICE("Finding all documents for board reports."))
         self.stdout.write(self.style.NOTICE(".........."))
-        self.findBoardReportPacket()
+        report_packet_raw = self.findBoardReportPacket()
+
+        # for idx, el in enumerate(report_packet_raw):
+        #     try:
+        #       self.makePacket(el[1])
+        #     except:
+        #       self.stdout.write(self.style.ERROR("Did not compile PDFs at index {0} for bill {1}").format(idx, el[0]))
+        #       pass
 
         self.stdout.write(self.style.NOTICE("Finding all documents for event agendas."))
         self.stdout.write(self.style.NOTICE(".........."))
-        self.findEventAgendaPacket()
+        event_packet_raw = self.findEventAgendaPacket()
+
+        # for idx, el in enumerate(event_packet_raw):
+        #     try:
+        #       self.makePacket(el[1])
+        #     except:
+        #       self.stdout.write(self.style.ERROR("Did not compile PDFs at index {0} for event {1}").format(idx, el[0]))
+        #       pass
 
         self.stdout.write(self.style.SUCCESS(".........."))
         self.stdout.write(self.style.SUCCESS("Job complete. Excellent work, everyone."))
@@ -39,14 +56,16 @@ class Command(BaseCommand):
         board_reports = self.connection.execute(sa.text(query))
 
         # Testing
-        for document in board_reports:
-            print(document)
+        # for document in board_reports:
+        #     print(document)
+
+        return board_reports
 
 
     def findEventAgendaPacket(self):
-
+        # If we want to include the note in a nested array: SELECT i.event_id, array_agg(array[d.note, d.url])
         query = '''
-        SELECT i.event_id, array_agg(distinct d.url)
+        SELECT i.event_id, array_agg(DISTINCT d.url)
         FROM councilmatic_core_billdocument AS d
         INNER JOIN councilmatic_core_eventagendaitem as i
         ON i.bill_id=d.bill_id
@@ -56,7 +75,23 @@ class Command(BaseCommand):
         event_agendas = self.connection.execute(sa.text(query))
 
         # Testing
-        for document in event_agendas:
-            print(document)
+        # for document in event_agendas:
+        #     print(document)
+
+        return event_agendas
+
+
+    def makePacket(self, filenames_collection):
+
+      merger = PdfFileMerger()
+
+      for filename in filenames_collection:
+          opened_url = urlopen(filename).read()
+          merger.append(BytesIO(opened_url), 'rb')
+
+      # This is a PdfFileMerger object, which can be written to a new file like so:
+      # merger.write("document-output.pdf")
+
+      return merger
 
 
