2366933028ddb22b450d58fa457020e4c414a449
reginafcompton <reginafcompton@gmail.com>
2017-03-22 15:42:17 -0500

    Fix crontasks

--- cron_tasks.sh
+++ cron_tasks.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
 cd /home/datamade/lametro-staging
-/home/datamade/.virtualenvs/lametro-staging/bin/python manage.py import_data >> /tmp/lametro-loaddata.log 2>&1
+/home/datamade/.virtualenvs/lametro-staging/bin/python manage.py import_data >> /tmp/lametro-staging-loaddata.log 2>&1
 /home/datamade/.virtualenvs/lametro-staging/bin/python manage.py update_index
+
+cd /home/datamade/lametro
+/home/datamade/.virtualenvs/lametro/bin/python manage.py import_data >> /tmp/lametro-loaddata.log 2>&1
+/home/datamade/.virtualenvs/lametro/bin/python manage.py update_index
