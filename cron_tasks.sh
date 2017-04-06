629a7777fbd70e7a4b298424505751b0eb77b94b
reginafcompton <reginafcompton@gmail.com>
2017-04-06 12:09:27 -0500

    New crontab

--- cron_tasks.sh
+++ cron_tasks.sh
@@ -3,3 +3,4 @@
 cd /home/datamade/lametro
 /home/datamade/.virtualenvs/lametro/bin/python manage.py import_data >> /tmp/lametro-loaddata.log 2>&1
 /home/datamade/.virtualenvs/lametro/bin/python manage.py update_index
+/home/datamade/.virtualenvs/lametro/bin/python manage.py compile_pdfs
