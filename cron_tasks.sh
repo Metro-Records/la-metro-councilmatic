c2d09da82b295b9780c78c8c18a5ff2c3d78e686
reginafcompton <reginafcompton@gmail.com>
2017-05-15 16:21:28 -0500

    Fix crontab

--- cron_tasks.sh
+++ cron_tasks.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-cd /home/datamade/lametro
-/home/datamade/.virtualenvs/lametro/bin/python manage.py import_data >> /tmp/lametro-loaddata.log 2>&1
-/home/datamade/.virtualenvs/lametro/bin/python manage.py compile_pdfs >> /tmp/lametro-compilepdfs.log 2>&1
-/home/datamade/.virtualenvs/lametro/bin/python manage.py update_index
+cd /home/datamade/lametro && /home/datamade/.virtualenvs/lametro/bin/python manage.py import_data >> /tmp/lametro-loaddata.log 2>&1
+cd /home/datamade/lametro && /home/datamade/.virtualenvs/lametro/bin/python manage.py compile_pdfs >> /tmp/lametro-compilepdfs.log 2>&1
+cd /home/datamade/lametro && /home/datamade/.virtualenvs/lametro/bin/python manage.py update_index
