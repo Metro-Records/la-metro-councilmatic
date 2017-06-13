5a1a1507a82409f483c90b64e848dbe41258cec6
reginafcompton <reginafcompton@gmail.com>
2017-06-13 17:40:25 -0500

    Begin

--- lametro/views.py
+++ lametro/views.py
@@ -148,6 +148,14 @@ class LAMetroEventDetail(EventDetailView):
             # Put results inside a list with assigned fields (from namedtuple)
             related_board_reports = [board_report_tuple(*r) for r in cursor_copy]
 
+            # Find agenda link.
+            # if event.documents.all():
+            #     for document in event.documents.all():
+            #         print(document.note, "note!!!")
+            #         if "Agenda" in document.note:
+            #             context['agenda_url'] = document.url
+            #             print(context['agenda_url'])
+
             context['related_board_reports'] = related_board_reports
 
         return context
