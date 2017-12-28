c5a0ee437b5d096f2e9a6ca0fa1fa2cad060455d
reginafcompton <reginafcompton@gmail.com>
2017-12-28 12:11:47 -0600

    Deployment: move PDFs before a deployment and change ownership of static repo to datamade

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -7,11 +7,15 @@ mkdir -p /home/datamade/la-metro-councilmatic
 
 if [ "$DEPLOYMENT_GROUP_NAME" == "staging" ]
 then
+    # Preserve uploaded PDFs
+    mv /home/datamade/lametro-staging/lametro/static/pdf/agenda-*.pdf /tmp
     rm -Rf /home/datamade/lametro-staging
     mkdir -p /home/datamade/lametro-staging
 fi
 if [ "$DEPLOYMENT_GROUP_NAME" == "production" ]
 then
+    # Preserve uploaded PDFs
+    mv /home/datamade/lametro/lametro/static/pdf/agenda-*.pdf /tmp
     rm -Rf /home/datamade/lametro
     mkdir -p /home/datamade/lametro
 fi
