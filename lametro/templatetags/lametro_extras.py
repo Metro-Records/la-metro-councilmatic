9c031ca91b74869cac221249a5e958940c357fee
reginafcompton <reginafcompton@gmail.com>
2018-08-13 16:21:21 -0500

    Add custom filter for finding relevant attachment text, and highlight it in the search results

--- lametro/templatetags/lametro_extras.py
+++ lametro/templatetags/lametro_extras.py
@@ -10,7 +10,7 @@ import urllib
 
 from councilmatic.settings_jurisdiction import *
 from councilmatic.settings import PIC_BASE_URL
-from councilmatic_core.models import Person, EventDocument
+from councilmatic_core.models import Person, EventDocument, Bill
 
 from lametro.models import LAMetroEvent
 from lametro.utils import format_full_text, parse_subject
@@ -191,3 +191,8 @@ def find_agenda_url(all_documents):
     valid_urls += pdf_url
 
     return valid_urls[0]
+
+@register.simple_tag
+def get_attachment_text(ocd_id):
+    bill_with_attachments = Bill.objects.get(ocd_id=ocd_id)
+    return ' '.join(d.full_text for d in bill_with_attachments.documents.all() if d.full_text)
