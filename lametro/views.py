213431257c67e5325fe290795992bf61733070c0
reginafcompton <reginafcompton@gmail.com>
2018-10-31 10:20:55 -0500

    Order memberships in the SQL

--- lametro/views.py
+++ lametro/views.py
@@ -524,9 +524,9 @@ class LACommitteeDetailView(CommitteeDetailView):
         with connection.cursor() as cursor:
             sql = ('''
               SELECT
+                m.role,
                 p.name, p.slug, p.ocd_id,
                 m.extras,
-                array_agg(m.role) as committee_role,
                 array_agg(mm.label::VARCHAR)
                 FILTER (WHERE mm.label is not Null) as label,
                 split_part(p.name, ' ', 2) AS last_name
@@ -548,40 +548,27 @@ class LACommitteeDetailView(CommitteeDetailView):
               AND m.end_date::date > NOW()::date
               AND m.role != 'Chief Executive Officer'
               GROUP BY
-                p.name, p.slug, p.ocd_id, m.extras
-              ORDER BY last_name
+                m.role, p.name, p.slug, p.ocd_id, m.extras
+              ORDER BY
+                    CASE
+                        WHEN m.role='Chair' THEN 0
+                        WHEN m.role='Vice Chair' THEN 1
+                        WHEN m.role='Member' THEN 2
+                    END
             ''')
 
             cursor.execute(sql, [settings.OCD_CITY_COUNCIL_ID, committee.ocd_id])
-
             columns = [c[0] for c in cursor.description]
-            columns.append('index')
-            cursor_copy = []
-
-            for obj in cursor:
-                if 'Chair' in obj[3]:
-                    obj = obj + ("1",)
-                elif '1st Vice Chair' in obj[3] or 'Vice Chair' in obj[3]:
-                    obj = obj + ("2",)
-                elif '2nd Vice Chair' in obj[3]:
-                    obj = obj + ("3",)
-                elif 'Nonvoting Member' in obj[3]:
-                    obj = obj + ("5",)
-                else:
-                    obj = obj + ("4",)
-                cursor_copy.append(obj)
-
-            results_tuple      = namedtuple('Member', columns)
-            objects_list       = [results_tuple(*r) for r in cursor_copy]
-            membership_objects = sorted(objects_list, key=lambda x: x[5])
+            results_tuple = namedtuple('Member', columns)
+            objects_list = [results_tuple(*r) for r in cursor]
 
             context['membership_objects'] = objects_list
 
             sql = ('''
               SELECT
+                m.role,
                 p.name, p.slug, p.ocd_id,
                 m.extras,
-                array_agg(m.role) as committee_role,
                 array_agg(mm.label::VARCHAR)
                 FILTER (WHERE mm.label is not Null) as label,
                 split_part(p.name, ' ', 2) AS last_name
@@ -603,30 +590,20 @@ class LACommitteeDetailView(CommitteeDetailView):
               AND m.end_date::date > NOW()::date
               AND m.role != 'Chief Executive Officer'
               GROUP BY
-                p.name, p.slug, p.ocd_id, m.extras
-              ORDER BY last_name
+                m.role, p.name, p.slug, p.ocd_id, m.extras
+              ORDER BY
+                CASE
+                    WHEN m.role='Chair' THEN 0
+                    WHEN m.role='Vice Chair' THEN 1
+                    WHEN m.role='Member' THEN 2
+                END
             ''')
 
             cursor.execute(sql, [settings.OCD_CITY_COUNCIL_ID, committee.ocd_id])
 
             columns = [c[0] for c in cursor.description]
-            columns.append('index')
-            cursor_copy = []
-
-            for obj in cursor:
-                if 'Chair' in obj[3]:
-                    obj = obj + ("1",)
-                elif '1st Vice Chair' in obj[3] or 'Vice Chair' in obj[3]:
-                    obj = obj + ("2",)
-                elif '2nd Vice Chair' in obj[3]:
-                    obj = obj + ("3",)
-                else:
-                    obj = obj + ("4",)
-                cursor_copy.append(obj)
-
-            results_tuple      = namedtuple('Member', columns)
-            objects_list       = [results_tuple(*r) for r in cursor_copy]
-            membership_objects = sorted(objects_list, key=lambda x: x[5])
+            results_tuple = namedtuple('Member', columns)
+            objects_list = [results_tuple(*r) for r in cursor]
 
             context['ad_hoc_list'] = objects_list
 
