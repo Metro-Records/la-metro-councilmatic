6711fe41ec8f2892bf6f4613dff30cb538846e6c
Hannah Cushman <hannah.cushman@gmail.com>
2019-05-20 14:34:19 -0500

    Continue implementation

--- lametro/static/js/utils.js
+++ lametro/static/js/utils.js
@@ -39,29 +39,7 @@ var SmartLogic = {
             console.log('Could not generate SmartLogic token');
         });
     },
-    hints: function(query) {
-        if ( window.localStorage.token == undefined ) {
-            SmartLogic.generateToken()
-        };
-
-        $.ajax({
-            type: 'GET',
-            // TO-DO: Figure out if this is the right URL
-            url: 'https://cloud.smartlogic.com/svc/0ef5d755-1f43-4a7e-8b06-7591bed8d453/ses/CombinedModel/hints/' + query + '.json',
-            beforeSend: function (xhr) {
-                xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-                xhr.setRequestHeader('Authorization', 'Bearer '+ window.localStorage.token);
-            },
-            statusCode: {
-                403: function (xhr, textStatus, errorThrown) {
-                    SmartLogic.generateToken();
-                    $.ajax(this);
-                }
-            }
-        }).done(function (response) {
-            return response;
-        }).error(function (xhr, textStatus, errorThrown) {
-            console.log('Could not retrieve hints for term ' + query);
-        });
+    buildServiceUrl: function(query) {
+        return 'https://cloud.smartlogic.com/svc/0ef5d755-1f43-4a7e-8b06-7591bed8d453/ses/CombinedModel/hints/' + query + '.json';
     }
 }
