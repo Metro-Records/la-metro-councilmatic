cb0ee144434af5d6a9153dcb4247b44caa703afd
hancush <hannah.cushman@gmail.com>
2019-10-15 15:44:05 -0500

    Show non-preferred term in preferred term suggestion if it matches the query

--- lametro/static/js/autocomplete.js
+++ lametro/static/js/autocomplete.js
@@ -34,28 +34,43 @@ function autocompleteSearchBar(element) {
       data: {}
     },
     paramName: '',
-    transformResult: function(response) {
+    transformResult: function(response, term) {
       var res = JSON.parse(response);
       var noneFoundText = "No suggestions found. Press enter to perform a keyword search."
       if (res.status_code == 500 || res.terms.length < 1) {
-        return {
-          suggestions: [noneFoundText]
-        };
+        return {suggestions: [noneFoundText]};
       } else {
         return {
           suggestions: $.map(res.terms, function(d) {
-              // TODO: If Metro prefers these results, refactor this to work
-              // with the /concepts result structure
-              /* If the search term is an acronym, displays that as a part of the suggestion */
-              //var nature = '';
-              //if (d.values[0]['nature'] == 'NPT') {
-              //  nature = d.values[0]['value'];
-              //  nature = ' (' + nature + ')';
-              //}
-              //return {'value': d.name + nature, 'data': d.id};
-              return {'value': d.term.name, 'data': d.term.id};
-            }
-          )
+            /* d.term.equivalence is an array of objects. Each object contains
+            a "fields" array, also an array of objects, containing alternative
+            names for the concept. Identify whether our search term matches one
+            of these labels, so we can include it in the suggestion. */
+            var nptLabel = '';
+
+            if ( d.term.equivalence !== undefined && d.term.equivalence.length > 0 ) {
+              var npt;
+
+              $.each(d.term.equivalence, function(idx, el) {
+                npt = el.fields.reduce(function(inp, el) {
+                  if (inp) {
+                    return inp
+                  } else {
+                    if (el.field.name.toLowerCase() == term.toLowerCase()) {
+                      return el.field.name;
+                    }
+                  }
+                }, undefined);
+
+                if ( npt ) {
+                  nptLabel = ' (' + npt + ')';
+                  return false; // Equivalent to "break"
+                }
+              });
+            };
+
+            return {'value': d.term.name + nptLabel, 'data': d.term.id};
+          })
         };
       }
     },
