f4c59883fc0f5d0385c6e4552904c16cc79a6498
hancush <hannah.cushman@gmail.com>
2019-12-20 15:42:26 -0600

    Don't check for table in release.sh

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,9 +5,4 @@ echo "Migrating database"
 python manage.py migrate --noinput
 python manage.py import_shapes data/boundary.geojson
 
-if [ "`psql ${DATABASE_URL} -tAX -c "SELECT COUNT(*) FROM opencivicdata_division"`" -eq "0" ]; then
-    echo "Initializing pupa divisions"
-    pupa dbinit us
-fi
-
 echo "Release finished"
