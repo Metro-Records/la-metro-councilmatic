03c603cbc2340db67cf6dad4e0c8a643c3682668
hancush <hannah.cushman@gmail.com>
2019-12-20 15:18:12 -0600

    Add import_shapes to release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,8 +3,9 @@ echo "Running release.sh"
 
 echo "Migrating database"
 python manage.py migrate --noinput
+python manage.py import_shapes data/boundary.geojson
 
-if [ `psql ${DATABASE_URL} -tAX -c "SELECT COUNT(*) FROM opencivicdata_division"` -eq "0" ]; then
+if [ "`psql ${DATABASE_URL} -tAX -c "SELECT COUNT(*) FROM opencivicdata_division"`" -eq "0" ]; then
     echo "Initializing pupa divisions"
     pupa dbinit us
 fi
