539b81fe1d440014b871c02b58040768fd56f5fb
hancush <hannah.cushman@gmail.com>
2019-12-20 12:05:38 -0600

    Re-enable release phase, add scrape task

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -x
-export HEROKU_DEBUG=1
-
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
+
+# Set ${TABLE} to the name of a table that you expect to have data in it.
+if [ `psql ${DATABASE_URL} -tAX -c "SELECT COUNT(*) FROM opencivicdata_division"` -eq "0" ]; then
+    pupa dbinit us
+fi
