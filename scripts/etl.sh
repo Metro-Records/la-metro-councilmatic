e4f8ecefdcce4007be4dc58fcd628d9436765126
hancush <hannah.cushman@gmail.com>
2019-12-26 12:39:45 -0600

    Add AWS settings to settings.py

--- scripts/etl.sh
+++ scripts/etl.sh
@@ -3,8 +3,8 @@
 # Location of manage.py in container
 MANAGE_PY="python /app/manage.py"
 
-$MANAGE_PY refresh_pic
-$MANAGE_PY compile_pdfs
-$MANAGE_PY convert_attachment_text
-$MANAGE_PY update_index --batch-size=50
+$MANAGE_PY refresh_pic && \
+$MANAGE_PY compile_pdfs && \
+$MANAGE_PY convert_attachment_text && \
+$MANAGE_PY update_index --batch-size=50 && \
 $MANAGE_PY data_integrity
