b0e8894b4cdce4dc3216c013cad30967110eb181
hancush <hannah.cushman@gmail.com>
2020-01-31 14:20:40 -0600

    Remove STATIC_ROOT setting

--- councilmatic/settings.py
+++ councilmatic/settings.py
@@ -109,7 +109,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MERGER_BASE_URL = 'https://metro-pdf-merger.datamade.us'
 # MERGER_BASE_URL = 'http://0.0.0.0:5000'
