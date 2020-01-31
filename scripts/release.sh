ad8df95aede393fdb2bb32c93317de8429da3552
hancush <hannah.cushman@gmail.com>
2020-01-31 13:14:36 -0600

    Revert "Merge pull request #531 from datamade/2.5_deploy"
    This reverts commit 0f37367848883f555283683cf84f5c39da967436, reversing
    changes made to 0ffef7b834d71f2a7122b43f9244ad423d23d32b.

--- scripts/release.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -x
-echo "Running release.sh"
-
-echo "Migrating database"
-python manage.py migrate --noinput
-python manage.py import_shapes data/boundary.geojson
-
-echo "Release finished"
