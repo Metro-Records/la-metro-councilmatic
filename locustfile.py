d2555bfeef3a80eb7fa529c879518eb9f923eddb
hancush <hannah.cushman@gmail.com>
2020-04-20 15:04:43 -0500

    Initialize load testing script

---
+++ locustfile.py
@@ -0,0 +1,18 @@
+from locust import HttpLocust, TaskSet, task, between
+
+
+class UserBehavior(TaskSet):
+    @task
+    def index(self):
+        self.client.get('/')
+
+    @task
+    def event_detail(self):
+        self.client.get('/event/regular-board-meeting-9db63964de28/')
+
+
+class WebsiteUser(HttpLocust):
+    host = 'https://lametro-upgrade.datamade.us'
+    task_set = UserBehavior
+    # Average time on page is 1:18
+    wait_time = between(60, 90)
