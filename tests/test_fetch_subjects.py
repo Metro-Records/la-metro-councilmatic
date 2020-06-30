b9ba35ddbacb45edfc1f0d243d8aa8e20c295cd4
hancush <hannah.cushman@gmail.com>
2020-06-30 15:18:12 -0500

    Update request parameter

--- tests/test_fetch_subjects.py
+++ tests/test_fetch_subjects.py
@@ -6,7 +6,7 @@ def test_fetch_subjects_from_related_terms(client, metro_subject):
 
     dummy_terms = [subject.name, 'some other subject']
 
-    response = client.get('/subjects/', {'related_terms': [subject.name, 'some other subject']})
+    response = client.get('/subjects/', {'related_terms[]': [subject.name, 'some other subject']})
     response = json.loads(response.content.decode('utf-8'))
 
     assert response['status_code'] == 200
