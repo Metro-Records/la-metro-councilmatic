59b3f0cd6744433ff1de2d1c4aa4a1fd654c8a54
hancush <hannah.cushman@gmail.com>
2020-07-10 14:44:00 -0500

    Remove decryption hook

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,8 +6,4 @@ if [ "$DJANGO_MANAGEPY_MIGRATE" = 'on' ]; then
     python manage.py import_shapes data/boundary.geojson
 fi
 
-if [ "$DECRYPT_SECRETS" = 'on' ]; then
-    blackbox_postdeploy
-fi
-
 exec "$@"
