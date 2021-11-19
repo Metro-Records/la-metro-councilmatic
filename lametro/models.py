aa2d54f76df3821aee5421edad346a32f47139d4
Fatima Gomez <fatima.gomez@datamade.us>
2021-11-19 09:51:32 -0600

    revise _status method logic

--- lametro/models.py
+++ lametro/models.py
@@ -142,7 +142,7 @@ class LAMetroBill(Bill, SourcesMixin):
         return self._status(description)
 
     def _status(self, description):
-        if description and BILL_STATUS_DESCRIPTIONS[description.upper()]:
+        if description and description.upper() in BILL_STATUS_DESCRIPTIONS.items():
             return BILL_STATUS_DESCRIPTIONS[description.upper()]['search_term']
         return None
 
