2b81d1ca2f05680ab81937ed1793788e8e8dde1c
msj <monkruman.st.jules@datamde.us>
2022-05-10 09:14:55 -0400

    Recent past meetings now returns all events in past two weeks

--- lametro/models.py
+++ lametro/models.py
@@ -493,11 +493,11 @@ class LAMetroEvent(Event, LiveMediaMixin, SourcesMixin):
         Returns meetings in the current month that have occured in the past
         two weeks.
         '''
-        current_month = timezone.now().month
         two_weeks_ago = timezone.now() - timedelta(weeks=2)
 
         meetings_in_past_two_weeks = cls.objects.with_media().filter(
-            start_time__month=current_month, start_time__gte=two_weeks_ago).order_by('-start_time')
+                start_time__gte=two_weeks_ago
+            ).order_by('-start_time')
 
         # since has_passed is a property of LAMetroEvent rather than
         # a model attribute, we have to make sure returned meetings 
