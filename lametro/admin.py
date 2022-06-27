3ce82af925ed1798a7f6dc6dc223a4c5bf443ddd
hancush <hannah.cushman@gmail.com>
2022-06-27 10:32:20 -0500

    Apply black

--- lametro/admin.py
+++ lametro/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+
 # import your models
 
 # Register your models here.
--- lametro/smartlogic.py
+++ lametro/smartlogic.py
@@ -5,18 +5,18 @@ from requests.exceptions import HTTPError
 
 
 class SmartLogic(object):
-    BASE_URL = 'https://cloud.smartlogic.com/'
-    SERVICE_URL = 'svc/0dcee7c7-1667-4164-81e5-c16e46f2f74c/ses/v1.2/CombinedModel/'
+    BASE_URL = "https://cloud.smartlogic.com/"
+    SERVICE_URL = "svc/0dcee7c7-1667-4164-81e5-c16e46f2f74c/ses/v1.2/CombinedModel/"
 
     def __init__(self, api_key):
         self.api_key = api_key
-        self._token = self.token()['access_token']
+        self._token = self.token()["access_token"]
 
     @property
     def auth_headers(self):
         return {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Authorization': 'Bearer {}'.format(self._token)
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Authorization": "Bearer {}".format(self._token),
         }
 
     def endpoint(self, method, route, **kwargs):
@@ -33,13 +33,13 @@ class SmartLogic(object):
             return response
 
     def token(self):
-        data = {'grant_type': 'apikey', 'key': self.api_key}
-        return self.endpoint('post', 'token', data=data)
+        data = {"grant_type": "apikey", "key": self.api_key}
+        return self.endpoint("post", "token", data=data)
 
     def terms(self, params):
         return self.endpoint(
-            'post',
-            self.SERVICE_URL + 'terms.json',
+            "post",
+            self.SERVICE_URL + "terms.json",
             params=params,
-            headers=self.auth_headers
+            headers=self.auth_headers,
         )
