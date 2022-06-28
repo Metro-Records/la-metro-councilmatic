b7640242597daf7cc0238cb1fb840aae6f76e81d
hancush <hannah.cushman@gmail.com>
2022-06-28 16:11:35 -0500

    Reference env var after conf loaded

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -5,10 +5,10 @@ set -euo pipefail
 # Make directory for project
 mkdir -p /home/datamade/lametro
 
+# Decrypt files encrypted with blackbox
+cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/ && chown -R datamade.datamade . && sudo -H -u datamade blackbox_postdeploy
+
 source /home/datamade/lametro/configs/$DEPLOYMENT_GROUP_NAME-config.conf
 
 # Preserve uploaded PDFs
 mv /home/datamade/$APP_NAME/lametro/static/pdf/agenda-*.pdf /tmp
-
-# Decrypt files encrypted with blackbox
-cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/ && chown -R datamade.datamade . && sudo -H -u datamade blackbox_postdeploy
