aabed627e2d7445993ac027e93de549a8df3bc47
hancush <hannah.cushman@gmail.com>
2022-06-28 15:35:15 -0500

    Add zero-downtime deployment scripts

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,35 +2,13 @@
 
 set -euo pipefail
 
-rm -Rf /home/datamade/la-metro-councilmatic
-mkdir -p /home/datamade/la-metro-councilmatic
+# Make directory for project
+mkdir -p /home/datamade/lametro
 
-# Make the directory for the ETL logs
-mkdir -p /var/log/councilmatic
-chown -R datamade.www-data /var/log/councilmatic
+source /home/datamade/lametro/configs/$DEPLOYMENT_GROUP_NAME-config.conf
 
-if [ "$DEPLOYMENT_GROUP_NAME" == "staging" ]
-then
-    # Preserve uploaded PDFs
-    if /home/datamade/lametro-staging/lametro/static/pdf/agenda-*.pdf
-    then
-      mv /home/datamade/lametro-staging/lametro/static/pdf/agenda-*.pdf /tmp
-    fi
-
-    rm -Rf /home/datamade/lametro-staging
-    mkdir -p /home/datamade/lametro-staging
-fi
-if [ "$DEPLOYMENT_GROUP_NAME" == "production" ]
-then
-    # Preserve uploaded PDFs
-    if /home/datamade/lametro/lametro/static/pdf/agenda-*.pdf
-    then
-      mv /home/datamade/lametro/lametro/static/pdf/agenda-*.pdf /tmp
-    fi
-
-    rm -Rf /home/datamade/lametro
-    mkdir -p /home/datamade/lametro
-fi
+# Preserve uploaded PDFs
+mv /home/datamade/$APP_NAME/lametro/static/pdf/agenda-*.pdf /tmp
 
 # Decrypt files encrypted with blackbox
 cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/ && chown -R datamade.datamade . && sudo -H -u datamade blackbox_postdeploy
