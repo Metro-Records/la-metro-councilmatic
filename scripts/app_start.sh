fd745a2de3bb52f1ca28f95f543035c466fc65ce
hancush <hannah.cushman@gmail.com>
2022-07-06 11:15:00 -0500

    🙏

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -58,10 +58,9 @@ service nginx reload || service nginx start
 # docker-compose.deployment.yml. Data should be persisted between containers,
 # thanks to our volume use.
 [ -n "$(docker ps -f NAME=solr-$DEPLOYMENT_GROUP_NAME -q)" ] && \
-    (cd $PROJECT_DIR; \
-    docker stop solr-$DEPLOYMENT_GROUP_NAME; \
-    docker rm solr-$DEPLOYMENT_GROUP_NAME)
+    (docker stop solr-$DEPLOYMENT_GROUP_NAME; docker rm solr-$DEPLOYMENT_GROUP_NAME)
 
+cd $PROJECT_DIR
 docker-compose -f docker-compose.deployment.yml up -d solr-$DEPLOYMENT_GROUP_NAME
 
 # It's safe to terminate the older version of the site
