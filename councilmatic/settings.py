26f19808819bf3b8b44cc2072dd32bb58e74686d
Forest Gregg <fgregg@datamade.us>
2023-01-18 15:00:01 -0500

    try using whitenoisesstorage

--- councilmatic/settings.py
+++ councilmatic/settings.py
@@ -138,6 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Third Party Keys
 RECAPTCHA_PUBLIC_KEY = env("RECAPTCHA_PUBLIC_KEY")
