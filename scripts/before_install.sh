833d3d720e4a1dc1adf738018a2e9edb8797d451
Forest Gregg <fgregg@datamade.us>
2023-01-18 21:49:12 -0500

    remove old configs

--- scripts/before_install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-# Make directory for project
-mkdir -p /home/datamade/la-metro-councilmatic
-
-# Decrypt files encrypted with blackbox
-cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/ && chown -R datamade.datamade . && sudo -H -u datamade blackbox_postdeploy
