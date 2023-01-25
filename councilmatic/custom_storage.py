12261d6561b218efb0b81ace2ee91423896a6bc4
Xavier Medrano <xaviermedrano@Xaviers-MacBook-Pro.local>
2023-01-25 11:18:03 -0500

    have bucket_name value change with environment

--- councilmatic/custom_storage.py
+++ councilmatic/custom_storage.py
@@ -1,4 +1,6 @@
 from storages.backends.s3boto3 import S3Boto3Storage
+from django.conf import settings
+
 
 class MediaStorage(S3Boto3Storage):
-    bucket_name = 'la-metro-headshots-staging'
+    bucket_name = settings.BUCKET_NAME
