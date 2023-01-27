124085b6d9df24b6bb3713ece95b881cf2478005
Xavier Medrano <xaviermedrano@Xaviers-MacBook-Pro.local>
2023-01-27 10:13:48 -0500

    add debugging statements

--- councilmatic/custom_storage.py
+++ councilmatic/custom_storage.py
@@ -2,5 +2,8 @@ from storages.backends.s3boto3 import S3Boto3Storage
 from django.conf import settings
 
 
+print("from custom_storage:", settings.BUCKET_NAME)
+
+
 class MediaStorage(S3Boto3Storage):
     bucket_name = settings.BUCKET_NAME
