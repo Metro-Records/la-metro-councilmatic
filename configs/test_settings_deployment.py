57bda959d740ec8131a0e4e794f29c2f5b2bfbfc
Xavier Medrano <xaviermedrano@Xaviers-MacBook-Pro.local>
2023-01-27 10:32:31 -0500

    remove debugging statements

--- configs/test_settings_deployment.py
+++ configs/test_settings_deployment.py
@@ -77,7 +77,6 @@ HEADSHOT_PATH = os.path.join(os.path.dirname(__file__), '..'
 SHOW_TEST_EVENTS = False
 
 BUCKET_NAME = 'la-metro-headshots-staging'
-print("from test_settings.deployment:", BUCKET_NAME)
 
 SMART_LOGIC_KEY = 'smartlogic api key'
 SMART_LOGIC_ENVIRONMENT = '0ef5d755-1f43-4a7e-8b06-7591bed8d453'
