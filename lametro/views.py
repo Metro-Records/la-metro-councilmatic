5ac183f754256c9520ee10d942893497f6a4c6e9
Xavier Medrano <xaviermedrano@Xaviers-MacBook-Pro.local>
2023-01-30 09:41:53 -0500

    have person/event templates print source from their context, remove OCD ID

--- lametro/views.py
+++ lametro/views.py
@@ -90,9 +90,6 @@ class LABillDetail(BillDetailView):
         context = super().get_context_data(**kwargs)
         bill = self.get_object()
 
-        sources = list(bill.sources.all())
-        context['sources'] = sources
-
         context['attachments'] = bill.attachments.all().order_by(Lower('note'))
 
         actions = bill.actions.all()
@@ -158,9 +155,6 @@ class LAMetroEventDetail(EventDetailView):
     def get_context_data(self, **kwargs):
         context = super(EventDetailView, self).get_context_data(**kwargs)
         event = context['event']
-
-        sources = list(event.sources.all())
-        context['sources'] = sources
 
         # Metro admins should see a status report if Legistar is down.
         # GET the calendar page, which contains relevant URL for agendas.
@@ -228,7 +222,6 @@ class LAMetroEventDetail(EventDetailView):
             context['url_form'] = AgendaUrlForm()
         if 'pdf_form' not in context:
             context['pdf_form'] = AgendaPdfForm()
-
 
         context['USING_ECOMMENT'] = settings.USING_ECOMMENT
 
@@ -595,9 +588,6 @@ class LAPersonDetailView(PersonDetailView):
 
         context = super().get_context_data(**kwargs)
         person = context['person']
-
-        sources = list(person.sources.all())
-        context['sources'] = sources
 
         council_post = person.latest_council_membership.post
 
