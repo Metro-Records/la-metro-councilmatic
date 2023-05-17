19d266bf4661f57a2636eb822acd433bec024aca
hannah cushman garland <hannah.cushman@gmail.com>
2023-05-17 10:38:15 -0500

    Only import shapes if asked

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,9 @@ set -e
 
 if [ "$DJANGO_MANAGEPY_MIGRATE" = 'on' ]; then
     python manage.py migrate --noinput
+fi
+
+if [ "$DJANGO_MANAGEPY_IMPORT_SHAPES" = 'on' ]; then
     python manage.py import_shapes data/final/boundary.geojson
 fi
 
