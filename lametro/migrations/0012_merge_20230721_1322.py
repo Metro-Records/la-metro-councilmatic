43bb22c899591bab8cef1a76fee2e2c61eee96dc
msj <monkruman.st.jules@datamde.us>
2023-07-21 16:27:25 -0400

    Format migration

--- lametro/migrations/0012_merge_20230721_1322.py
+++ lametro/migrations/0012_merge_20230721_1322.py
@@ -4,7 +4,6 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("lametro", "0010_alter_eventbroadcast_observed"),
         ("lametro", "0011_relatebillsubject"),
