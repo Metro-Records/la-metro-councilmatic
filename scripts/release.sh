34d0f63566e5f994ec615e7d7c85faf52a125cb7
Hannah Cushman Garland <hannah.cushman@datamade.us>
2024-11-21 10:48:33 -0600

    Move pipefail back

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-set -euo pipefail
-
 if [ -n "${PRODUCTION}" ] || [ -n "${STAGING}" ]; then
+    set -euo pipefail
+
     python manage.py migrate --noinput
     python manage.py createcachetable
     python manage.py import_shapes data/final/boundary.geojson
