eb7c1f497642d149465de1f54cbb1ceca7bcfe44
Quarto GHA Workflow Runner <quarto-github-actions-publish@example.com>
2024-12-02 15:14:48 +0000

    Built site for gh-pages

--- lametro/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin  # noqa
-from lametro.models import Alert
-
-
-class AlertAdmin(admin.ModelAdmin):
-    list_display = ("type", "description")
-
-
-admin.site.register(Alert, AlertAdmin)
