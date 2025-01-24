5558a0a151900552a1e86c043a54ab05532e6387
Hannah Cushman Garland <hannah.cushman@datamade.us>
2025-01-24 09:52:01 -0600

    Add Metro users, full About page text to content fixture
    Update review app default settings, richtext image CSS

--- wagtail.mk
+++ wagtail.mk
@@ -7,6 +7,8 @@ cms_data : db
 
 legislative_data : db
 	docker compose run --rm -e DATABASE_URL=${DATABASE_URL} scrapers
+	docker compose run --rm -e DATABASE_URL=${DATABASE_URL} python manage.py refresh_guid
+	docker compose run --rm -e DATABASE_URL=${DATABASE_URL} python manage.py update_index
 
 db :
 	if [ -z "${DATABASE_URL}" ]; then \
