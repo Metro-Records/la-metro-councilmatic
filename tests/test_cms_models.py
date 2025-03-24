76330449738cf15527e92bf6b897e68aae94709b
Xavier Medrano <xaviermedrano@Xaviers-MacBook-Pro.local>
2025-03-24 12:36:59 -0400

    add test for tooltip display

--- tests/test_cms_models.py
+++ tests/test_cms_models.py
@@ -3,6 +3,8 @@ import pytest
 from django.urls import reverse
 from freezegun import freeze_time
 
+from lametro.models import Tooltip
+
 
 @freeze_time("2025-03-15 10:00:00")
 @pytest.mark.parametrize(
@@ -25,3 +27,23 @@ def test_alert_expiration(client, alert, expiration_str, should_display):
     assert (
         test_alert.description in response.content.decode("utf-8")
     ) == should_display
+
+
+@pytest.mark.parametrize(
+    "disabled,should_display",
+    [(False, True), (True, False)],
+)
+def test_tooltips_display(client, disabled, should_display, bill, metro_subject):
+    """
+    Test that tooltips display when their target is present, and when not disabled.
+    """
+    metro_subject.build(name="Kiosks")
+
+    # Add this classification to make sure the bill is discoverable by the model manager
+    test_bill = bill.build(classification=["Board Box"], subject=["Kiosks"])
+    test_content = "A test tooltip for a subject!"
+    Tooltip.objects.create(target="Subject", content=test_content, disabled=disabled)
+
+    url = reverse("lametro:bill_detail", kwargs={"slug": test_bill.slug})
+    response = client.get(url)
+    assert (test_content in response.content.decode("utf-8")) == should_display
