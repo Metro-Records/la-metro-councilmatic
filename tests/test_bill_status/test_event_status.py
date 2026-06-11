6e0c390b630415da6bc3938e3165f122a5a65bc2
Lily Ye <lily.ye@datamade.us>
2026-06-11 12:46:07 -0500

    Make bills unique with separate agenda items

--- tests/test_bill_status/test_event_status.py
+++ tests/test_bill_status/test_event_status.py
@@ -1,4 +1,6 @@
 import pytest
+from django.utils import timezone
+
 from lametro.services import EventService
 from opencivicdata.legislative.models import BillVersion
 
@@ -9,6 +11,7 @@ def test_event_action_description(
     first_org,
     first_event_date,
     first_agenda_item,
+    another_first_agenda_item,
     bill,
     bill_action,
     event_related_entity,
@@ -23,8 +26,16 @@ def test_event_action_description(
         note="Board Report",
         date=first_event_date,
     )
+    bill_info = {
+        "id": "ocd-bill/2436c8c9-564f-4cdd-a2ce-bcfe082de2c2",
+        "title": "A unique bill",
+        "created_at": timezone.now(),
+        "updated_at": timezone.now(),
+        "identifier": "2019-0686",
+        "slug": "2019-0686",
+    }
+    bill_with_no_action = bill.build(**bill_info)
 
-    bill_with_no_action = bill.build()
     BillVersion.objects.create(
         bill=bill_with_no_action,
         note="Board Report",
@@ -39,17 +50,31 @@ def test_event_action_description(
         description="Pick me",
     )
 
-    event_related_entity.build(agenda_item=first_agenda_item, bill=bill_with_action)
-    event_related_entity.build(agenda_item=first_agenda_item, bill=bill_with_no_action)
+    agenda_item_with_bill_action = first_agenda_item
+    agenda_item_with_no_bill_action = another_first_agenda_item
+
+    event_related_entity.build(
+        agenda_item=agenda_item_with_bill_action, bill=bill_with_action
+    )
+    event_related_entity.build(
+        agenda_item=agenda_item_with_no_bill_action, bill=bill_with_no_action
+    )
 
     qs = EventService.get_related_board_reports(first_event)
     agenda_items = list(qs)
-    bill_with_action_result = (
-        agenda_items[0].related_entities.get(bill=bill_with_action).bill
-    )
-    bill_with_no_action_result = (
-        agenda_items[0].related_entities.get(bill=bill_with_no_action).bill
-    )
 
-    assert bill_with_action_result.event_action_description == "Pick me"
-    assert bill_with_no_action_result.event_action_description is None
+    agenda_item_with_description = agenda_items[0]
+    agenda_item_with_no_description = agenda_items[1]
+
+    assert (
+        agenda_item_with_description.related_entities.all()[
+            0
+        ].bill.event_action_description
+        == "Pick me"
+    )
+    assert (
+        agenda_item_with_no_description.related_entities.all()[
+            0
+        ].bill.event_action_description
+        is None
+    )
