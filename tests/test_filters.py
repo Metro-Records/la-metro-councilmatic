fa5cd31f57cb27d691564d4ec38af8264e974697
Lily Ye <lily.ye@datamade.us>
2026-06-11 16:09:26 -0500

    Update filter name
    See #1278

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -1,18 +1,18 @@
 from lametro.templatetags.lametro_extras import (
     inferred_status_label,
-    bill_status_from_last_action,
+    bill_status_from_event_action,
 )
 
 
 def test_no_label_when_no_action():
-    status = bill_status_from_last_action(None)
+    status = bill_status_from_event_action(None)
     html = inferred_status_label(status)
 
     assert html is None
 
 
 def test_label_when_action():
-    status = bill_status_from_last_action("adopTEd as amenDed")
+    status = bill_status_from_event_action("adopTEd as amenDed")
     html = inferred_status_label(status)
 
     assert html == "<span class='label label-adopted'>Adopted</span>"
