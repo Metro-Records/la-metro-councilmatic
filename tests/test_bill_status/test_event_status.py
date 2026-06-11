c5186b5a0d02feaf3efb2296cf280aa01cf687e1
Lily Ye <lily.ye@datamade.us>
2026-06-11 16:49:00 -0500

    Add tests for get_related_board_reports

--- tests/test_bill_status/test_event_status.py
+++ tests/test_bill_status/test_event_status.py
@@ -6,7 +6,73 @@ from opencivicdata.legislative.models import BillVersion
 
 
 @pytest.mark.django_db
-def test_event_action_description(
+def test_get_related_board_reports_excludes_restricted_bills(
+    bill,
+    bill_action,
+    event_related_entity,
+    first_org,
+    first_event,
+    first_event_date,
+    first_agenda_item,
+):
+    """
+    Exclude restricted bills.
+    """
+    restricted_bill = bill.build(extras={"restrict_view": True})
+
+    BillVersion.objects.create(
+        bill=restricted_bill,
+        note="Board Report",
+        date=first_event_date,
+    )
+
+    bill_action.build(
+        bill=restricted_bill,
+        organization=first_org,
+        date=first_event_date,
+        order=1,
+        description="Don't look at me",
+    )
+
+    event_related_entity.build(agenda_item=first_agenda_item, bill=restricted_bill)
+
+    qs = EventService.get_related_board_reports(first_event)
+    agenda_items = list(qs)
+    assert agenda_items == []
+
+
+@pytest.mark.django_db
+def test_get_related_board_reports_excludes_bills_with_no_BillVersion(
+    bill,
+    bill_action,
+    event_related_entity,
+    first_org,
+    first_event,
+    first_event_date,
+    first_agenda_item,
+):
+    """
+    Exclude bills without a BillVersion. May be sign of incomplete import.
+    """
+    restricted_bill = bill.build()
+
+    bill_action.build(
+        bill=restricted_bill,
+        organization=first_org,
+        date=first_event_date,
+        order=1,
+        description="Don't look at me",
+    )
+
+    event_related_entity.build(agenda_item=first_agenda_item, bill=restricted_bill)
+
+    qs = EventService.get_related_board_reports(first_event)
+    agenda_items = list(qs)
+    assert agenda_items == []
+
+
+@pytest.mark.django_db
+def test_only_annotates_events_with_actions(
     first_event,
     first_org,
     first_event_date,
@@ -17,7 +83,9 @@ def test_event_action_description(
     event_related_entity,
 ):
     """
-    Test event action description only annotates bill with actions.
+    Test that LABillManager.with_event_action_description() only
+    annotates bills with actions, and returns None for bills with no
+    actions, as called by EventService.get_related_board_reports(event).
     """
 
     bill_with_action = bill.build()
@@ -34,8 +102,8 @@ def test_event_action_description(
         "identifier": "2019-0686",
         "slug": "2019-0686",
     }
-    bill_with_no_action = bill.build(**bill_info)
 
+    bill_with_no_action = bill.build(**bill_info)
     BillVersion.objects.create(
         bill=bill_with_no_action,
         note="Board Report",
@@ -77,4 +145,53 @@ def test_event_action_description(
             0
         ].bill.event_action_description
         is None
+    )
+
+
+@pytest.mark.django_db
+def test_annotates_latest_action_when_multiple_actions_with_the_same_date(
+    first_org,
+    first_event,
+    first_event_date,
+    first_agenda_item,
+    bill,
+    bill_action,
+    event_related_entity,
+):
+    """
+    Test that annotation is the action with highest order if there
+    are multiple actions from the same event.
+    """
+
+    bill = bill.build()
+    BillVersion.objects.create(
+        bill=bill,
+        note="Board Report",
+        date=first_event_date,
+    )
+
+    bill_action.build(
+        bill=bill,
+        organization=first_org,
+        date=first_event_date,
+        order=1,
+        description="Don't pick me",
+    )
+
+    bill_action.build(
+        bill=bill,
+        organization=first_org,
+        date=first_event_date,
+        order=2,
+        description="Pick me",
+    )
+
+    event_related_entity.build(agenda_item=first_agenda_item, bill=bill)
+
+    qs = EventService.get_related_board_reports(first_event)
+    agenda_items = list(qs)
+
+    assert (
+        agenda_items[0].related_entities.all()[0].bill.event_action_description
+        == "Pick me"
     )
