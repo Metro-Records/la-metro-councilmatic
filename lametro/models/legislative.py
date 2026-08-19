cb8f54f9df29e28dba82c575cf36570497ce2e29
Lily Ye <lily.ye@datamade.us>
2026-08-19 12:08:55 -0700

    Filter bills, not orgs, by test org status
    Actually we don't really query for organizations themselves anywhere except the
    committee page, where the `committee` query + the committee view logic
    effectively eliminate all test bodies.

--- lametro/models/legislative.py
+++ lametro/models/legislative.py
@@ -100,6 +100,19 @@ class LAMetroBillManager(models.Manager):
         """
         qs = super().get_queryset()
 
+        TEST_ORGS = [
+            "zTESTz OBS Test Committee",
+            "zTESTz Finance, Budget and Audit Committee",
+            "TO BE REMOVED",
+            "VCM Draft Meeting",
+            "Test - Live Regular Meeting Test",
+            "Test - Live Committee Meeting Test",
+        ]
+
+        test_orgs = Q(from_organization__name__in=TEST_ORGS) | Q(
+            from_organization__extras__bodytype="Test Committee"
+        )
+
         on_published_agenda = Q(
             eventrelatedentity__agenda_item__event__status="passed"
         ) | Q(eventrelatedentity__agenda_item__event__status="cancelled")
@@ -110,6 +123,7 @@ class LAMetroBillManager(models.Manager):
 
         qs = (
             qs.exclude(extras__restrict_view=True)
+            .exclude(test_orgs)
             .annotate(
                 board_box=Case(
                     When(
@@ -958,50 +972,11 @@ class EventRelatedEntity(EventRelatedEntity):
     bill = ProxyForeignKey(LAMetroBill, null=True, on_delete=models.SET_NULL)
 
 
-class LAMetroOrganizationManager(models.Manager):
-    """
-    Handle the display of different sets of organizations depending on the
-    context, especially hiding test committees.
-    """
-
-    def get_queryset(self):
-
-        qs = super().get_queryset()
-
-        TEST_ORGS = [
-            "zTESTz OBS Test Committee",
-            "zTESTz Finance, Budget and Audit Committee",
-            "TO BE REMOVED",
-            "VCM Draft Meeting",
-            "Test - Live Regular Meeting Test",
-            "Test - Live Committee Meeting Test",
-        ]
-
-        qs = qs.annotate(
-            test_org=Case(
-                When(
-                    organization__name__in=TEST_ORGS,
-                    then=True,
-                ),
-                When(
-                    organization__extras__bodytype__contains=["Test Committee"],
-                    then=True,
-                ),
-                default=False,
-                output_field=models.BooleanField(),
-            )
-        )
-
-        return qs
-
-
 class LAMetroOrganization(Organization, SourcesMixin):
     """
     Overrides use the LAMetroEvent object, rather than the default Event
     object, so test events are hidden appropriately.
     """
-
-    objects = LAMetroOrganizationManager()
 
     class Meta:
         proxy = True
